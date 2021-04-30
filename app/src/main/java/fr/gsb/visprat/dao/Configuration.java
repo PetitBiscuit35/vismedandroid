e2a2541a9c60f85da668cc928e4b5ad83ad63d09
alevesque <arthur.levesque35@gmail.com>
2021-04-30 14:33:10 +0200

    configuration

--- app/src/main/java/fr/gsb/visprat/dao/Configuration.java
+++ app/src/main/java/fr/gsb/visprat/dao/Configuration.java
@@ -2,8 +2,8 @@ package fr.gsb.visprat.dao;
 
 public class Configuration {
     private static String protocol = "http";
-    private static String host = "100.115.28.104";
-    private static String path = "sio2/arthurlevesque/WS-VisMed";
+    private static String host = "192.168.0.34";
+    private static String path = "sio2/LEVESQUE/WS-VisMed";
 
     /**
      * Fournit l'URL complète d'accès à l'API-Rest
