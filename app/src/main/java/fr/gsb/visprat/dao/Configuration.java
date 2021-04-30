b23b77ef161a497f3da7776af7e8d5395af4dffe
Simon Tessard <simon.tessard@gmail.com>
2021-04-30 16:31:43 +0200

    Update Config

--- app/src/main/java/fr/gsb/visprat/dao/Configuration.java
+++ app/src/main/java/fr/gsb/visprat/dao/Configuration.java
@@ -2,10 +2,9 @@ package fr.gsb.visprat.dao;
 
 public class Configuration {
 
-    private static String protocol = "http";
-    private static String host = "192.168.0.27";
-    private static String path = "sio2/MONTEIL/WS-VisMed";
-
+    private static String protocol = "https";
+    private static String host = "192.168.0.23";
+    private static String path = "sio2/simon/WS-VisMed";
 
     /**
      * Fournit l'URL complète d'accès à l'API-Rest
