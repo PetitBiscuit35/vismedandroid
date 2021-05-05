2572defbaf0d15d63d70bea11c77df335352f3a3
Simon Tessard <simon.tessard@gmail.com>
2021-05-05 16:08:40 +0200

    Si tous les champs ne sont pas renseignés - bouton ajouter désactivé

--- app/src/main/java/fr/gsb/visprat/AddRapportVisiteActivity.java
+++ app/src/main/java/fr/gsb/visprat/AddRapportVisiteActivity.java
@@ -2,6 +2,8 @@ package fr.gsb.visprat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -27,7 +29,34 @@ public class AddRapportVisiteActivity extends AppCompatActivity {
         editTextCoefConfiance = (EditText) findViewById(R.id.editTextCoefConfiance);
         editTextIdMotifVisite = (EditText) findViewById(R.id.editTextIdMotifVisite);
 
+        // Désactivation du bouton ajouter
         buttonAjouter = (Button) findViewById(R.id.buttonAjouter);
+        buttonAjouter.setEnabled(false);
+
+        // Ecouteurs
+        editTextIdMedecin.addTextChangedListener(new CheckIfEmpty());
+        editTextDateVisite.addTextChangedListener(new CheckIfEmpty());
+        editTextDateCreaRapport.addTextChangedListener(new CheckIfEmpty());
+        editTextBilan.addTextChangedListener(new CheckIfEmpty());
+        editTextCoefConfiance.addTextChangedListener(new CheckIfEmpty());
+        editTextIdMotifVisite.addTextChangedListener(new CheckIfEmpty());
+    }
+
+    private class CheckIfEmpty implements TextWatcher
+    {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Si les 2 champs ne sont pas renseignés le bouton de validation n'est pas activé
+                if (editTextIdMedecin.getText().length() > 0 && editTextDateVisite.getText().length() > 0
+                    && editTextDateCreaRapport.getText().length() > 0 && editTextBilan.getText().length() > 0
+                    && editTextCoefConfiance.getText().length() > 0 && editTextIdMotifVisite.getText().length() > 0)
+                { buttonAjouter.setEnabled(true); }
+                else { buttonAjouter.setEnabled(false); }
+        }
+        @Override
+        public void afterTextChanged(Editable editable) { }
     }
 }
 
